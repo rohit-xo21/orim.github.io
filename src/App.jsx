1862bbe25876963cf2a79407c6a1793650ceba3f
Rohit <rohithrajka524@gmail.com>
2025-11-28 23:03:20 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css';
 
 import LightRays from './components/LightRays/LightRays';
 
+let rohitImg = 'https://res.cloudinary.com/doapnwi6d/image/upload/v1764350902/rohit_om4iur.svg';
+
 function App() {
   return (
     <div className="App">
@@ -10,17 +12,17 @@ function App() {
         raysOrigin="top-center"
         raysColor="#ffffff"
         raysSpeed={1}
-        lightSpread={1}
-        rayLength={2}
-        pulsating={false}
-        fadeDistance={1.0}
+        lightSpread={0.5}
+        rayLength={0.5}
+        pulsating={true}
+        fadeDistance={0.5}
         saturation={1.0}
         followMouse={true}
         mouseInfluence={0.1}
         noiseAmount={0.0}
         distortion={0.0}
       />
-      <img src="/rohit.svg" alt="Rohit" className="rohit-image" />
+      <img src={rohitImg} alt="Rohit" className="rohit-image" />
       <h1 className="name">Rohithraj</h1>
     </div>
   );
